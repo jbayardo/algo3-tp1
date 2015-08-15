60ba49fe605dcd34a87c9a80ee0463267b0ff304
Chris <chriscuneo93@gmail.com>
2015-08-15 19:35:03 -0300

    Main Input

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
+#include <fstream>
+#include "Camino.h"
+#include "Median.h"
+#include "Exploradoras.h"
+#include <tuple>
+
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+int main(int argc, char * argvs[]) {
+	if (argc != 4){
+		cout << "usage: " << argvs[0] << " numeroDeEjercicio, archivoEntrada, archivoSalida" << endl;
+		return 0;
+	}
+
+	ifstream input;
+	input.open(argvs[2], ifstream::in);
+	ofstream output;
+	output.open(argvs[3], ofstream::out);
+
+	if (!input.is_open()){
+		cout << "File not found" << endl;
+		return 0;
+	}
+
+	switch(atoi(argvs[1])){
+		case 1:{
+			Camino ej1;
+			while (!input.eof()){
+				int metros;
+				vector<int> cities;				
+				input >> metros;
+				input.ignore();
+				cout << "Metros= "<< metros << endl;
+				while ((( input.peek() != '\n' ) || ( input.peek() != '\r' )) && !input.eof()){
+					int km;
+					input >> km;
+					cities.push_back(km);
+					cout << km << " ";
+				}
+				cout << endl;
+				int res = ej1.greedy(metros, cities);
+				output << res << endl;
+				cout << res << endl << endl;
+			}
+			break;}
+		case 2:{
+			Median ej2;
+
+			break;}
+		case 3:{
+			Exploradoras ej3;
+
+			break;}
+		default:
+
+			cout << "usage: " << argvs[0] << " numeroDeEjercicio(1, 2, 3)" << endl;
+			break;
+	}
     return 0;
 }
