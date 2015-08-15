8d492e9ad8d43576d9ef2c1618d4daf1580f7c4d
Izikiel <ezequiel.gambaccini@gmail.com>
2015-08-15 20:03:37 -0300

    Renombro variables

--- src/Camino.cpp
+++ src/Camino.cpp
@@ -1,32 +1,32 @@
 #include "Camino.h"
 
 std::tuple<int, int> Camino::greedy(int metros, const std::vector<int> &distancias) {
-    int i = 0; // Comienzo del mejor camino
-    int j = 0; // Fin del mejor camino
+    int best_start = 0; // Comienzo del mejor camino
+    int best_end = 0; // Fin del mejor camino
 
-    int k = 0; // Comienzo del camino actual
+    int start = 0; // Comienzo del camino actual
 
-    for (int m = 0; m < distancias.size(); ++m) {
-        if (distancias[m] - distancias[k] > metros) {
+    for (int end = 0; end < distancias.size(); ++end) {
+        if (distancias[end] - distancias[start] > metros) {
             // Terminamos con este camino, porque no podemos agregar mas ciudades.
 
-            if ((m - k) >= (j - i)) {
+            if ((end - start) >= (best_end - best_start)) {
                 // Si este camino es mejor que el que ya teniamos, lo guardamos.
-                i = k;
-                j = m;
+                best_start = start;
+                best_end = end;
             }
 
             // Borro cualquier cantidad de greedy que sean necesarias para poder continuar buscando.
-            while (k < m && distancias[m] - distancias[k] > metros) {
-                ++k;
+            while (start < end && distancias[end] - distancias[start] > metros) {
+                ++start;
             }
 
         } else {
             // Encontramos un mejor camino, extendiendo el que ya teniamos
-            i = k;
-            j = m;
+            best_start = start;
+            best_end = end;
         }
     }
 
-    return std::make_pair(i, j);
+    return std::make_pair(best_start, best_end);
 }
