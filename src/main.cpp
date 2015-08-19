3bfbbd92d015e44dcca355692e1b67a3fb43649e
Chris <chriscuneo93@gmail.com>
2015-08-18 23:21:35 -0300

    Listo input ej3

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "Median.h"
 #include "Exploradoras.h"
 #include <tuple>
+#include <sstream>
 
 
 using namespace std;
@@ -45,12 +46,38 @@ int main(int argc, char * argvs[]) {
 			}
 			break;
 		} case 2: {
-			Median ej2;
+			while (!input.eof()) {
+				Median ej2;
+				list<int> numbers;
 
+				while (input.peek() != std::char_traits<char>::to_int_type('\r') && !input.eof()) {
+					int number;
+					input >> number;
+					numbers.push_back(number);
+				}
+
+				list<int> res = ej2.insert(numbers);
+				for (auto it : res) {
+					output << it << " ";	//Ver si no jode el ultimo espacio
+				}
+				output << endl;
+			}
 			break;
 		} case 3: {
-			Exploradoras ej3;
-
+			string line;
+			while (getline(input, line)) {
+				Exploradoras ej3;
+				vector<pair<char, string>> exploradoras;
+				istringstream line(line);
+				string friendship;
+				while (getline(line, friendship, ';')) {
+					char exploradora = friendship.front();
+					friendship.erase(0, 2);
+					exploradoras.push_back(make_pair(exploradora, friendship));
+				}
+				std::pair<int, std::string> res = ej3.backtracking(exploradoras);
+				output << res.second << endl;
+			}
 			break;
 		} default:
 			cout << "usage: " << argvs[0] << " numeroDeEjercicio(1, 2, 3)" << endl;
